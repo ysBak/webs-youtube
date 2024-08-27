e32fac93a5020a5b75f8a9cc564146ad4fdc50f7
30308-78\3030878 <bys4034@gmail.com>
2024-08-27 16:32:58 +0900

    Work main section content

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { developerText } from '../../data/developer'
+import { Link } from 'react-router-dom'
 
 const Developer = () => {
   return (
-    <div>Developer</div>
+    <section id='developer'>
+      <h2>おすすめチャンネルの紹介です。</h2>
+      <div className='developer__inner overflow'>
+        {developerText.map((developer, key) => (
+          <div className='developer' key={key}>
+            <div className='developer__img play__icon'>
+              <Link to={`/chennel/${developer.chennelId}`}>
+              <img src={developer.img} alt={developer.name} />
+              </Link>
+            </div>
+            <div className='developer__info'>
+              <Link to={`/chennel/${developer.chennelId}`}>
+                  {developer.name}
+              </Link>
+          </div>
+        </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
+import { developerText } from '../data/developer'
+import { Link } from 'react-router-dom'
+
 const Developer = () => {
   return (
     <Main 
     title="おすすめチャンネル" 
     description="おすすめチャンネル Page" 
     >
-      Developer
+      <section id='developer'>
+      <h2>おすすめチャンネルの紹介です。</h2>
+      <div className='developer__inner'>
+        {developerText.map((developer, key) => (
+          <div className='developer' key={key}>
+            <div className='developer__img play__icon'>
+              <Link to={`/chennel/${developer.chennelId}`}>
+              <img src={developer.img} alt={developer.name} />
+              </Link>
+            </div>
+            <div className='developer__info'>
+              <Link to={`/chennel/${developer.chennelId}`}>
+                  {developer.name}
+              </Link>
+          </div>
+        </div>
+        ))}
+      </div>
+
+
+      </section>
     </Main>
   )
 }
